9a980bfdc1f623737433f819fc8855b6920c33dd
DamlaOksel <damlaoksel@hotmail.com>
2023-10-20 17:12:18 +0300

    comments, photos ve footer kısmı yapıldı

--- Odevler/fox-university/scripts/swiper.js
+++ Odevler/fox-university/scripts/swiper.js
@@ -3,3 +3,19 @@ var swiper = new Swiper(".mySwiper", {
       el: ".swiper-pagination",
     },
   });
+  var swiper1 = new Swiper(".mySwiper1", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
