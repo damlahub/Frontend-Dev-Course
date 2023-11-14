9d72453eb4ab52d606c789871ba59611e4b41ca3
damlaoksel <damlaaoksel@gmail.com>
2023-11-14 16:35:30 +0300

    Medium Odev | Sağ aside'ye sticky eklendi ve headera scroll jsi yazıldı

---
+++ Odevler/medium/scroll.js
@@ -0,0 +1,21 @@
+let header = document.querySelector("#header");
+let intro = document.querySelector("#intro");
+header.style.zIndex="55";
+
+let headerDistance = intro.offsetHeight;
+console.log(headerDistance);
+
+window.onscroll = function () {
+    FixedScroll();
+}
+
+function FixedScroll() {
+    if (window.scrollY > headerDistance) {
+        header.querySelector(".row").classList.remove("bg-warning");
+        header.querySelector(".row").classList.add("bg-white");
+        header.style.top = "0";
+    } else {
+        header.querySelector(".row").classList.add("bg-warning");
+        header.querySelector(".row").classList.remove("bg-white");
+    }
+}
